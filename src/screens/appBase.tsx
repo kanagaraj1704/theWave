68a24a7f026e916abe9ef9bdbbf657b0ce60bcc8
kanagaraj <kanagaraj@nanonino.in>
2024-04-22 16:34:59 +0530

    first init

---
+++ src/screens/appBase.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native"
+import { RegisterScreen } from "./registerScreen"
+
+export const Appbase = () => {
+
+
+    return (
+        <View>
+            <RegisterScreen />
+        </View>
+    )
+}
